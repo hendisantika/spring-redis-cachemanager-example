ef7c3144d56c9adbebc36a0ee4022d4cca453f7d
Hendi Santika <hendisantika@gmail.com>
2018-04-09 20:31:13 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendi/redismanager/RedisManagerApplication.java
@@ -0,0 +1,12 @@
+package com.hendi.redismanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisManagerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hendi/redismanager/RedisManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendi.redismanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
