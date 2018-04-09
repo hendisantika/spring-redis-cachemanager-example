262a0426d46a37f4a76bb7c234d379377a10e8f4
Hendi Santika <hendisantika@gmail.com>
2018-04-09 20:57:38 +0700

    #6 - Add Cache Config

--- src/main/java/com/hendi/redismanager/RedisManagerApplication.java
+++ src/main/java/com/hendi/redismanager/RedisManagerApplication.java
@@ -2,11 +2,23 @@ package com.hendi.redismanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class RedisManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisManagerApplication.class, args);
+
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        MusicService musicService = ctx.getBean(MusicService.class);
+
+        System.out.println("message: " + musicService.play("trumpet"));
+        System.out.println("message: " + musicService.play("trumpet"));
+        System.out.println("message: " + musicService.play("guitar"));
+        System.out.println("message: " + musicService.play("guitar"));
+
+        System.out.println("Done.");
     }
 }
