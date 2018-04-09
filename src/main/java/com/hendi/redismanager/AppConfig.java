568ec533868daaf8b90b24fec6180b780eb7b41b
Hendi Santika <hendisantika@gmail.com>
2018-04-09 20:54:39 +0700

    #5 - Add App Config

---
+++ src/main/java/com/hendi/redismanager/AppConfig.java
@@ -0,0 +1,61 @@
+package com.hendi.redismanager;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : redis-manager
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/04/18
+ * Time: 20.34
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableCaching
+@ComponentScan("com.hendi.redismanager")
+@PropertySource("classpath:/redis.properties")
+public class AppConfig {
+
+    private @Value("${redis.host}")
+    String redisHost;
+    private @Value("${redis.port}")
+    int redisPort;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName(redisHost);
+        factory.setPort(redisPort);
+        factory.setUsePool(true);
+        return factory;
+    }
+
+    @Bean
+    RedisTemplate<Object, Object> redisTemplate() {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<Object, Object>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
+
+    @Bean
+    CacheManager cacheManager() {
+        return new RedisCacheManager(redisTemplate());
+    }
+}
